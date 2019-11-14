dec7b57206bf4a56eb7c5a2c9dc112a31c6ddfe5
1399833047@qq.com <1399833047@qq.com>
2019-11-14 21:38:29 +0800

    完成不刷新页面登陆

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,15 @@ function post() {
                 $("#comment_section").hide();
             }
             else{
-                alert(response.message)
+                if(response.code == 2003){
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=ab964e0524966e490960&redirect_uri=http://localhost:8001/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable","true");//跳转页面后存入一个变量名为closable设置为true
+                    }
+                }else {
+                    alert(response.message)
+                }
             }
             console.log(response);
         },
